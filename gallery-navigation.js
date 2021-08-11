d639f392e1a55c6833d5be825cb862c4e3011070
silviu03 <83119817+silviu03@users.noreply.github.com>
2021-08-11 22:48:38 +0300

    update

--- gallery-navigation.js
+++ gallery-navigation.js
@@ -1,25 +1,25 @@
 $(function () {
 
-    // const home = $('.home-wrapper');
-    // const hairStyles = $(".hair-styles-wrapper");
-    // const meniuItems = $('.menu-item');
-    // const navPages = $('.target-item');
+    const home = $('.home-wrapper');
+    const hairStyles = $(".hair-styles-wrapper");
+    const meniuItems = $('.menu-item');
+    const navPages = $('.target-item');
 
-    // meniuItems.click(function (e) {
-    //     let data = $(this).data('link-item');
+    meniuItems.click(function (e) {
+        let data = $(this).data('link-item');
 
-    //     navPages.each(function (index, item) {
+        navPages.each(function (index, item) {
 
-    //         if (!$(item).hasClass('hidden')) {
-    //             $(item).addClass('hidden')
-    //         }
+            if (!$(item).hasClass('hidden')) {
+                $(item).addClass('hidden')
+            }
 
-    //         if (data == index) {
-    //             $(item).removeClass('hidden')
-    //         }
-    //     })
-        
-    // })
+            if (data == index) {
+                $(item).removeClass('hidden')
+            }
+        })
+
+    })
 
     // <----------contact-lens------------->
 
@@ -28,8 +28,8 @@ $(function () {
     let contactLens = $("#contact-lens-wrapper");
     const arrows = overlayContactLens.find("i");
     const contactLensIdArr = [];
-    
-    let getRandomInt = function(max) {
+
+    let getRandomInt = function (max) {
         return Math.floor(Math.random() * Math.floor(max));
     };
 
@@ -40,8 +40,8 @@ $(function () {
         contactLensIdArr.push(lensItem.id);
     })
 
-    contactLens.on('click', '.contact-lens-item', function() {
-        imgLensWrapper.css({backgroundImage: `url(poze/${$(this).data('img')}`})
+    contactLens.on('click', '.contact-lens-item', function () {
+        imgLensWrapper.css({ backgroundImage: `url(poze/${$(this).data('img')}` })
         imgLensWrapper.data('id', $(this).data('id'));
 
         // overlayContactLens.removeClass('hidden');
@@ -50,15 +50,15 @@ $(function () {
 
     let galleryIterator = contactLensIdArr.indexOf($(this).data('id'));
 
-    setInterval(function() {
+    setInterval(function () {
         if (galleryIterator >= contactLensIdArr.length) {
             galleryIterator = 0;
         } else {
             galleryIterator++;
         }
 
-        for(let i = 0; i < contactLensArr.length; i++) {
-            if(contactLensIdArr[getRandomInt(contactLensArr.length - 1)] === contactLensArr[i].id) {
+        for (let i = 0; i < contactLensArr.length; i++) {
+            if (contactLensIdArr[getRandomInt(contactLensArr.length - 1)] === contactLensArr[i].id) {
                 imgLensWrapper.attr('style', `background-image: url(poze/${contactLensArr[i].imgUrl})`);
                 imgLensWrapper.attr('data-id', contactLensArr[i].id);
             }
@@ -66,23 +66,23 @@ $(function () {
     }, 2000);
 
 
-    arrows.click(function(ev) {
+    arrows.click(function (ev) {
         ev.stopPropagation();
         let direction = $(this).data('direction');
         let currentLensId = $(this).parents('.overlay-contact-lens').find(".lens-img-wrapper").data('id');
 
         let galleryIterator = contactLensIdArr.indexOf(currentLensId);
 
-        switch(direction) {
-            case 'right': 
-                if(galleryIterator >= contactLensIdArr.length - 1) {
+        switch (direction) {
+            case 'right':
+                if (galleryIterator >= contactLensIdArr.length - 1) {
                     galleryIterator = 0;
                 } else {
                     galleryIterator++;
                 }
                 break;
             case 'left':
-                if(galleryIterator <= 0) {
+                if (galleryIterator <= 0) {
                     galleryIterator = contactLensIdArr.length - 1;
                 } else {
                     galleryIterator--;
@@ -90,20 +90,89 @@ $(function () {
                 break;
         }
 
-        for(let i = 0; i < contactLensArr.length; i++) {
-            if(contactLensIdArr[galleryIterator] === contactLensArr[i].id) {
+        for (let i = 0; i < contactLensArr.length; i++) {
+            if (contactLensIdArr[galleryIterator] === contactLensArr[i].id) {
                 imgLensWrapper.attr('style', `background-image: url(poze/${contactLensArr[i].imgUrl})`);
                 imgLensWrapper.attr('data-id', contactLensArr[i].id);
             }
         }
     })
 
-    overlayContactLens.click(function() {
+    overlayContactLens.click(function () {
         // $(this).addClass('hidden')
         $(this).fadeOut();
     })
 
 
+
+    // <---------------hair-style-------------->
+
+    let favoritesAdded = [];
+    const hairStylesWrapper = $('.hair-styles-wrapper'),
+        favoritesContentWrapper = $('.favorites'),
+        favoritesProperties = $('.favorites-properties'),
+        favoritesHaderIcon = $('.favorites-item'),
+        removeFavoritesBtn = $('#remove'),
+        getPropertyHTML = function (styleTypeObj) {
+            return `<div class="single-property-wrapper" data-id=${styleTypeObj.id}>
+            <div class="hair-style-info" >
+                <span>${styleTypeObj.description}</span>
+                <i class="fas fa-heart heart-toggle"></i>
+            </div>
+            <div class="img-wrapper hair2" style="background-image: url('poze/${styleTypeObj.imgUrl}")'></div>
+            <div class="hair-style-info">
+                <span>Something</span>
+                <span>${styleTypeObj.currency} ${styleTypeObj.price}</span>
+            </div>
+        </div>`
+        }
+
+    for (let i = 0; i < styleTypeArr.length; i++) {
+        let styleTypeObj = styleTypeArr[i],
+            styleTypeHTML = getPropertyHTML(styleTypeObj);
+        hairStylesWrapper.append(styleTypeHTML);
+    }
+
+    hairStylesWrapper.on('click', '.heart-toggle', function() {
+        const id = $(this).parents('.single-property-wrapper').data('id');
+
+        $(this).toggleClass('selected');
+        
+        if(jQuery.inArray(id, favoritesAdded) === -1) {
+            favoritesAdded.push(id);
+        } else {
+            favoritesAdded.splice(favoritesAdded.indexOf(id), 1);
+        }
+        console.log(favoritesAdded)
+
+        let favoritesTotal = $('.favorites-number');
+        
+        if(favoritesAdded.length > 0) {
+            favoritesTotal.text(favoritesAdded.length);
+            favoritesTotal.removeClass('hidden');
+            
+        } else {
+            favoritesTotal.addClass('hidden');
+            
+        }
+    })
+
+    favoritesHaderIcon.click(function(event) {
+        event.preventDefault();
+
+        if(favoritesAdded.length > 0) {
+            favoritesProperties.empty();
+            favoritesAdded.forEach(function(item) {
+                let addedContent = getPropertyHTML(styleTypeArr[item]);
+                favoritesProperties.append(addedContent);
+
+            }) 
+        }
+    }) 
+
+    removeFavoritesBtn.click(function() {
+        favoritesProperties.empty();
+    })
 
 
 
---
+++ hair-style-data.js
@@ -0,0 +1,45 @@
+let styleTypeArr = [
+    {
+        id: 1,
+        price: 60,
+        currency: '$',
+        description: 'hair style 1',
+        imgUrl: 'hair1.webp'
+    },
+    {
+        id: 2,
+        price: 70,
+        currency: '$',
+        description: 'hair style 2',
+        imgUrl: 'hair2.jpg'
+    },
+    {
+        id: 3,
+        price: 80,
+        currency: '$',
+        description: 'hair style 3',
+        imgUrl: 'hair3.jpg'
+    },
+    {
+        id: 4,
+        price: 90,
+        currency: '$',
+        description: 'hair style 4',
+        imgUrl: 'hair4.jpg'
+    },
+    {
+        id: 5,
+        price: 110,
+        currency: '$',
+        description: 'hair style 5',
+        imgUrl: 'hair5.jpg'
+    },
+    {
+        id: 6,
+        price: 130,
+        currency: '$',
+        description: 'hair style 6',
+        imgUrl: 'hair6.jpg'
+    },
+    
+]
